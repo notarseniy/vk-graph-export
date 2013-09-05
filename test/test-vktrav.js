f4947b2daea771a01a239fc863ca626ea68bbe4b
kolen <incredible.angst@gmail.com>
2013-09-06 00:30:02 +0400

    Draft data

--- test/test-vktrav.js
+++ test/test-vktrav.js
@@ -1,5 +1,19 @@
 
-
+bdate: "20.10.1991"
+city: "2"
+country: "1"
+first_name: "Fgsfds"
+home_phone: "+728346763254"
+id: 2617
+last_name: "Lastname"
+nickname: "Ololosha"
+online: 1
+photo_50: "http://cs234765.vk.me/v45384574/5be7/CCDHDC_xw.jpg"
+relation: "4"
+relation_partner: Object
+screen_name: "tetetest"
+sex: 1
+timezone: 3
 
 describe("VK friends traversing module", function() {
 
