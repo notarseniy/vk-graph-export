d1a7f82dc3ba3a3ca0f24574aeb05b975edfd5ea
Konstantin Mochalov <incredible.angst@gmail.com>
2013-09-06 18:00:42 +0400

    users_in_queue

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ window.vk_graph_export = {}
 window.vk_graph_export.Model = function() {
 	this.ready = ko.observable(false)
 	this.resulting_file_content = ko.observable(null)
+	this.users_in_queue = ko.observable(0)
+	
 }
